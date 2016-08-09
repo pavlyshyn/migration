96aeccf6bbac1118b86f836d439368476c4fc895
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-08-10 00:03:30 +0300

    init

--- src/Migration.php
+++ src/Migration.php
@@ -9,6 +9,15 @@
 namespace Pavlyshyn;
 
 class Migration {
+    
+    protected $adapter = null;
+    protected $directory = null;
+
+
+    public function __construct(Pavlyshyn\Orm $adapter, $directory = null) {
+        $$this->adapter = $adapter;
+        $$this->directory = $directory;
+    }
 
     public function up() {
         
