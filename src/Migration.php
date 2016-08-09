34f7d39bd442de4d7318a09a9a170b8c8e0e1e29
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2016-08-09 23:55:33 +0300

    init

---
+++ src/Migration.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the pavlyshyn/migration package
+ * 
+ * @author Roman Pavlyshyn <roman@pavlyshyn.com>
+ */
+
+namespace Pavlyshyn;
+
+class Migration {
+
+    public function up() {
+        
+    }
+
+    public function down() {
+        
+    }
+
+}
